4b1bff274e339480d9e90747e237eee65948cca2
aptheparker <aptheparker@gmail.com>
2023-08-25 17:57:33 +0900

    recommend page layout continue

--- src/pages/RecommendPage.jsx
+++ src/pages/RecommendPage.jsx
@@ -1,9 +1,16 @@
+// styles
+import classes from "./RecommendPage.module.css";
+
 const RecommendPage = () => {
   return (
-    <div>
-      <h1>RecommendPage</h1>
+    <div className={classes["recommend-page-container"]}>
+      <div className={classes["recommend-title-container"]}>
+        <div className={classes["recommend-title__text"]}>Recosk</div>
+      </div>
+      <h1>원하시는 종류를 선택해주세요</h1>
+      <button>선택완료</button>
     </div>
   );
-}
+};
 
 export default RecommendPage;
