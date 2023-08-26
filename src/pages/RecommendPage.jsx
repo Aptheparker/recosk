d30b4c1847a93815ee7379eee7f774f3e38200f8
aptheparker <aptheparker@gmail.com>
2023-08-27 07:59:33 +0900

    selection function added

--- src/pages/RecommendPage.jsx
+++ src/pages/RecommendPage.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 // styles
 import classes from "./RecommendPage.module.css";
 
@@ -9,8 +11,24 @@ import Icecream from "../assets/icecream.png";
 import Cake from "../assets/cake.png";
 import Bingsu from "../assets/bingsu.png";
 
-
 const RecommendPage = () => {
+  const [selectedMenu, setSelectedMenu] = useState("");
+
+  const handleMenuClick = (menu) => {
+    if (selectedMenu === menu) {
+      setSelectedMenu(null);
+    } else {
+      setSelectedMenu(menu);
+    }
+  };
+
+  const getMenuClassName = (menu) => {
+    if (selectedMenu === menu) {
+      return classes["selected-menu"];
+    }
+    return classes["image-container"];
+  };
+
   return (
     <div className={classes["recommend-page-container"]}>
       <div className={classes["recommend-title-container"]}>
@@ -18,32 +36,50 @@ const RecommendPage = () => {
       </div>
       <h1>원하시는 종류를 선택해주세요</h1>
       <div className={classes["menus-container"]}>
-        <div className={classes['image-container']}>
+        <div
+          className={getMenuClassName(Coffee)}
+          onClick={() => handleMenuClick(Coffee)}
+        >
           <img src={Coffee} alt="" />
-          <p className={classes['menu-name']}>커피</p>
+          <p className={classes["menu-name"]}>커피</p>
         </div>
-        <div className={classes['image-container']}>
+        <div
+          className={getMenuClassName(Tea)}
+          onClick={() => handleMenuClick(Tea)}
+        >
           <img src={Tea} alt="" />
-          <div className={classes['menu-name']}>차</div>
+          <div className={classes["menu-name"]}>차</div>
         </div>
-        <div className={classes['image-container']}>
+        <div
+          className={getMenuClassName(Juice)}
+          onClick={() => handleMenuClick(Juice)}
+        >
           <img src={Juice} alt="" />
-          <div className={classes['menu-name']}>주스</div>
+          <div className={classes["menu-name"]}>주스</div>
         </div>
-        <div className={classes['image-container']}>
+        <div
+          className={getMenuClassName(Icecream)}
+          onClick={() => handleMenuClick(Icecream)}
+        >
           <img src={Icecream} alt="" />
-          <div className={classes['menu-name']}>아이스크림</div>
+          <div className={classes["menu-name"]}>아이스크림</div>
         </div>
-        <div className={classes['image-container']}>
+        <div
+          className={getMenuClassName(Cake)}
+          onClick={() => handleMenuClick(Cake)}
+        >
           <img src={Cake} alt="" />
-          <div className={classes['menu-name']}>케이크</div>
+          <div className={classes["menu-name"]}>케이크</div>
         </div>
-        <div className={classes['image-container']}>
+        <div
+          className={getMenuClassName(Bingsu)}
+          onClick={() => handleMenuClick(Bingsu)}
+        >
           <img src={Bingsu} alt="" />
-          <div className={classes['menu-name']}>빙수</div>
+          <div className={classes["menu-name"]}>빙수</div>
         </div>
       </div>
-      <button className={classes['select-btn']}>선택완료</button>
+      <button className={classes["select-btn"]}>선택완료</button>
     </div>
   );
 };
