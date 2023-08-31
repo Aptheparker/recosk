f4066ed61bdb612fd0c46ac388392e51c71a6944
aptheparker <aptheparker@gmail.com>
2023-08-31 23:08:01 +0900

    selectedMenu Page updating

--- src/pages/SelectedMenuPage.jsx
+++ src/pages/SelectedMenuPage.jsx
@@ -1,11 +1,23 @@
+// components
+import Header from "../components/Layout/Header";
+import SelectedDescription from "../assets/descriptions/selected_description.png";
+import SelectButton from "../components/Common/SelectButton";
+
+// css
+import classes from "./SelectedMenuPage.module.css";
+
 const SelectedMenuPage = () => {
-	return (
-		<div>
-			<div>header</div>
-			<div>메뉴를 추천드립니다.</div>
-			<div></div>
-		</div>
-	);
+  return (
+    <div className={classes["page-container"]}>
+      <Header />
+      <img src={SelectedDescription} alt="selected-description" />
+      <div className={classes["menu-container"]}></div>
+			<div className={classes['buttons']}>
+				<SelectButton />
+				<SelectButton />
+			</div>
+    </div>
+  );
 };
 
 export default SelectedMenuPage;
