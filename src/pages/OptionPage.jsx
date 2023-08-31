644e92301154cd8f7e0aca363a3d7491a82b1d06
Taegon <taegon@cusee.net>
2023-08-31 13:46:00 +0900

    Revert "Revert "pull and reducer""
    This reverts commit 4578babe558c1c5f54d988a39e4251499c49c136.

--- src/pages/OptionPage.jsx
+++ src/pages/OptionPage.jsx
@@ -1,11 +1,11 @@
-import { SelectedList } from "../context/SelectedList";
+import { SelectedList } from '../context/SelectedList';
 
 //hook
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 // classes
-import classes from "./OptionPage.module.css";
+import classes from './OptionPage.module.css';
 
 // images
 import Chocolate from "../assets/ingredients/chocolate.png";
@@ -25,47 +25,48 @@ import Nuts from '../assets/taste/nuts.png';
 
 
 const OptionPage = () => {
-  const selectionContext = useContext(SelectedList);
-  const ingredients = selectionContext.ingredients;
-  const fruits = selectionContext.fruits;
-  const taste = selectionContext.taste;
+	const selectionContext = useContext(SelectedList);
+	const ingredients = selectionContext.ingredients;
+	const fruits = selectionContext.fruits;
+	const taste = selectionContext.taste;
 
-  const setFruits = (data) => {
-    if (fruits.includes(data)) {
-      selectionContext.setFruits({ type: "DELETE", payload: data });
-    } else {
-      selectionContext.setFruits({ type: "ADD", payload: data });
-    }
-  };
+	const setFruits = (data) => {
+		if (fruits.includes(data)) {
+			selectionContext.setFruits({ type: 'DELETE', payload: data });
+		} else {
+			selectionContext.setFruits({ type: 'ADD', payload: data });
+		}
+	};
 
-  const setIngredients = (data) => {
-    if (ingredients.includes(data)) {
-      selectionContext.setIngredients({ type: "BAN", payload: data });
-    } else {
-      selectionContext.setIngredients({ type: "ADD", payload: data });
-    }
-  };
+	const setIngredients = (data) => {
+		if (ingredients.includes(data)) {
+			selectionContext.setIngredients({ type: 'DELETE', payload: data });
+		} else {
+			selectionContext.setIngredients({ type: 'ADD', payload: data });
+		}
+	};
 
-  const setTaste = (data) => {
-    if (taste.includes(data)) {
-      selectionContext.setTaste({ type: "DELETE", payload: data });
-    } else {
-      selectionContext.setTaste({ type: "ADD", payload: data });
-    }
-  };
+	const setTaste = (data) => {
+		if (taste.includes(data)) {
+			selectionContext.setTaste({ type: 'DELETE', payload: data });
+		} else {
+			selectionContext.setTaste({ type: 'ADD', payload: data });
+		}
+	};
 
-  const onClickIngreButtonHandler = (e) => {
-    setIngredients(e.target.id);
-  };
+	const onClickIngreButtonHandler = (e) => {
+		setIngredients(e.target.id);
+	};
 
-  const onClickFruitsButtonHandler = (e) => {
-    setFruits(e.target.id);
-  };
+	const onClickFruitsButtonHandler = (e) => {
+		setFruits(e.target.id);
+	};
 
-  const onClickTasteButtonHandler = (e) => {
-    setTaste(e.target.id);
-  };
+	const onClickTasteButtonHandler = (e) => {
+		setTaste(e.target.id);
+	};
 
+<<<<<<< Updated upstream
   console.log(ingredients);
   console.log(taste);
   return (
@@ -150,6 +151,50 @@ const OptionPage = () => {
       </Link>
     </div>
   );
+=======
+	console.log(ingredients);
+	console.log(fruits);
+	console.log(taste);
+	return (
+		<div className={classes['page-container']}>
+			<div className={classes['title']}>Recosk</div>
+			<div className={classes['description']}>옵션을 선택해주세요</div>
+			<div>
+				<button id='milk' onClick={onClickIngreButtonHandler}>
+					milk
+				</button>
+				<button id='chocolate' onClick={onClickIngreButtonHandler}>
+					chocolate
+				</button>
+				<button id='caffeine' onClick={onClickIngreButtonHandler}>
+					caffeine
+				</button>
+				<button id='apple' onClick={onClickFruitsButtonHandler}>
+					apple
+				</button>
+				<button id='mango' onClick={onClickFruitsButtonHandler}>
+					mango
+				</button>
+				<button id='grapes' onClick={onClickFruitsButtonHandler}>
+					grapes
+				</button>
+				<button id='sweat' onClick={onClickTasteButtonHandler}>
+					달달
+				</button>
+				<button id='bitter' onClick={onClickTasteButtonHandler}>
+					씀
+				</button>
+				<button id='sour' onClick={onClickTasteButtonHandler}>
+					셔요
+				</button>
+
+				<Link to={'/loading'}>
+					<button>선택완료</button>
+				</Link>
+			</div>
+		</div>
+	);
+>>>>>>> Stashed changes
 };
 
 export default OptionPage;
