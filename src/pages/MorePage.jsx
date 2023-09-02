0a6a7076f1f493dd5106e24424719de21d3af034
Taegon <taegon@cusee.net>
2023-09-02 20:31:20 +0900

    api key update

--- src/pages/MorePage.jsx
+++ src/pages/MorePage.jsx
@@ -59,7 +59,8 @@ const MorePage = () => {
 		console.log(menus);
 
 		const apiUrl = 'https://api.openai.com/v1/chat/completions';
-		const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+		// const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+		const apiKey = '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';
 
 		const requestBody = {
 			model: 'gpt-3.5-turbo',
